9efea3b800db0fa5b602b4e6e56bee009ad69d11
PabloTercero <pabtercan@alu.edu.gva.es>
2026-05-19 11:20:31 +0200

    Interfaz grafica y launcher para lanzar la misma

--- src/main/java/com/emergencias/main/Main.java
+++ src/main/java/com/emergencias/main/Main.java
@@ -1,11 +1,29 @@
-package com.emergencias.main;
+package com.emergencias;
 
-import com.emergencias.controller.EmergencyManager;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        // Cargamos el diseño de SceneBuilder
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/MenuPrincipal.fxml"));
+
+        // ajustamos el tamaño de 400x400
+        Scene scene = new Scene(fxmlLoader.load(), 400, 400);
+
+        // Configuramos la ventana
+        stage.setTitle("🏔️ Sistema de Emergencias");
+        stage.setScene(scene);
+        stage.show(); // ¡Mostramos la ventana!
+    }
+
     public static void main(String[] args) {
-        // creamos el manager y deberia funcionar
-        EmergencyManager sistema = new EmergencyManager();
-        sistema.startSystem();
+        // En vez de lanzar la consola, lanzamos la app gráfica
+        launch(args);
     }
 }
